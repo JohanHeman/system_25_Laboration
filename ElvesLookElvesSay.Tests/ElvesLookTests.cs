1336729d4839ad062daaff99a145dbd89d7a70b1
JohanH <jojo2001@hotmail.se>
2026-09-17 15:08:07 +0200

    detta ändrade detta
    varför gjorde jah detta jdjadkjw dlkajwdlkwjdlwaj dlwaj dlwajadkawdjwkjd
    aw

--- ElvesLookElvesSay.Tests/ElvesLookTests.cs
+++ ElvesLookElvesSay.Tests/ElvesLookTests.cs
@@ -2,5 +2,18 @@ namespace ElvesLookElvesSay.Tests;
 
 public class ElvesLookTests
 {
+
     
+    [Fact]
+    public void CountNumberOcurrance_OneShouldReturnOneOne()
+    {
+        // Arrange
+        var sut = new LookSay();
+        var expected = 11;
+        // Act
+        var actual = sut.CountNumberOcurrance(1);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 }
