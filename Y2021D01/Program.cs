e4fb46df534e1ca0b13d3486a09a9e276766f008
JohanH <jojo2001@hotmail.se>
2026-09-17 14:43:38 +0200

    Changed program to take valid input file and pass advent of code test

--- Y2021D01/Program.cs
+++ Y2021D01/Program.cs
@@ -1,14 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using _2015DayOne;
 using AdventOfCode.Common;
 using Y2021D01;
+using Console = System.Console;
 
-Console.WriteLine("Hello, World!");
-var path = Path.Combine(AppContext.BaseDirectory, "input.txt");
-var text = File.ReadAllText(path);
+// Console.WriteLine("Hello, World!");
+// var path = Path.Combine(AppContext.BaseDirectory, "input.txt");
+// var text = File.ReadAllText(path);
 
-var depths = Input.Numbers(text);
-var sonar = new Sonar();
-var increases = sonar.CountIncreases(depths);
+// var depths = Input.Numbers(text);
+// var sonar = new Sonar();
+// var increases = sonar.CountIncreases(depths);
+//
+// Console.WriteLine($"The file contained {depths.Length} entries, and they increased {increases} times.");
+//
 
-Console.WriteLine($"The file contained {depths.Length} entries, and they increased {increases} times.");
+var path = Path.Combine(AppContext.BaseDirectory, "ElevatorInput.txt");
+
+var input = File.ReadAllText(path).Trim();
+
+var floorTracker = new FloorTracker();
+
+var result = floorTracker.TrackFloor(input);
+Console.WriteLine(result);
