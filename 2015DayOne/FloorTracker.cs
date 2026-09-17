6e84ed473f0dcb1c83ce26378f744ab7fd648f3f
JohanH <jojo2001@hotmail.se>
2026-09-17 13:25:35 +0200

    implement trackfloor function to pass the tests.

--- 2015DayOne/FloorTracker.cs
+++ 2015DayOne/FloorTracker.cs
@@ -2,4 +2,16 @@ namespace _2015DayOne;
 
 public class FloorTracker
 {
+    public static int TrackFloor(string path)
+    {
+        var splitPath = path.Split().ToArray();
+        var finallFloor = 0;
+        foreach (var floor in splitPath)
+        {
+            if (floor == "(") finallFloor += 1;
+            if (floor == ")") finallFloor -= 1;
+        }
+
+        return finallFloor;
+    }
 }
