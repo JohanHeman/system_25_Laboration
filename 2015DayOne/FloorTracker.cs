9d9325f645e99a2f7c0d35723a1ed45600f1ac42
JohanH <jojo2001@hotmail.se>
2026-09-17 14:22:57 +0200

    Implement logic for passing empty string input test

--- 2015DayOne/FloorTracker.cs
+++ 2015DayOne/FloorTracker.cs
@@ -4,6 +4,8 @@ public class FloorTracker
 {
     public int TrackFloor(string path)
     {
+
+        if (String.IsNullOrEmpty(path)) throw new Exception("The input can not be empty");
         var splitPath = path.ToCharArray();
         int finalFloor = 0;
         foreach (var floor in splitPath)
