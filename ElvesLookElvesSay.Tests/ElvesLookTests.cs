a5980c636b5e3f0920c1d39759da5747299f00d9
JohanH <jojo2001@hotmail.se>
2026-09-18 10:59:47 +0200

    broke test

--- ElvesLookElvesSay.Tests/ElvesLookTests.cs
+++ ElvesLookElvesSay.Tests/ElvesLookTests.cs
@@ -11,7 +11,7 @@ public class ElvesLookTests
         var sut = new LookSay();
         var expected = 11;
         // Act
-        var actual = sut.CountNumberOcurrance(1);
+        var actual = 4;
 
         // Assert
         Assert.Equal(expected, actual);
